7c76bc1ea77f58718fe656d143324f75055fccf5
nikitakozlovcoder <nikiton2606@mail.ru>
2018-04-04 16:05:34 +0300

    Expand button

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
 
-function onScroll() {
-    var nav = document.getElementsByClassName('header_nav_main')[0];
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+var expanded = true;
+window.onload = function () {
 
-    scrolled !== 0 ? nav.classList.add('nav_light') : nav.classList.remove('nav_light');
-}
+    window.addEventListener('scroll', onScroll);
+
+    function onScroll() {
+        var nav = document.getElementsByClassName('header_nav_main')[0];
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+        scrolled !== 0 ? nav.classList.add('nav_light') : nav.classList.remove('nav_light');
+    }
+
+    var expandGrid = document.getElementsByClassName('showcase_grid_expand')[0];
+
+    expandGrid.onclick = function () {
+        expanded = !expanded;
+        var grid = document.getElementsByClassName('showcase_grid')[0];
+        var showcaseOverflow = document.getElementsByClassName('showcase_overflow')[0];
+
+        if(!expanded) {
+
+            var height = grid.clientHeight;
+            showcaseOverflow.style.height = 150 + height + "px";
+            expandGrid.innerHTML = "Свернуть...";
+            expandGrid.style.padding = "5px";
+            console.log(height);
+        }
+        else{
+            showcaseOverflow.style.height = "400px";
+            expandGrid.innerHTML = "Показать все...";
+            expandGrid.style.padding = "15px";
+        }
+
+    };
+};
 
 
-window.addEventListener('scroll', onScroll);
+
 
 
 
