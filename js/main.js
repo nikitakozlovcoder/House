661c763bb6fe4eb9a910cac89a589bef4fe99982
nikitakozlovcoder <nikiton2606@mail.ru>
2018-04-05 11:35:31 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -1,39 +1,30 @@
 
+
+
 var expanded = true;
-window.onload = function () {
+var expandGrid = document.getElementsByClassName('showcase_grid_expand')[0];
 
-    window.addEventListener('scroll', onScroll);
+expandGrid.onclick = function () {
+    expanded = !expanded;
+    var grid = document.getElementsByClassName('showcase_grid')[0];
+    var showcaseOverflow = document.getElementsByClassName('showcase_overflow')[0];
 
-    function onScroll() {
-        var nav = document.getElementsByClassName('header_nav_main')[0];
-        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if(!expanded) {
 
-        scrolled !== 0 ? nav.classList.add('nav_light') : nav.classList.remove('nav_light');
-    }
-
-    var expandGrid = document.getElementsByClassName('showcase_grid_expand')[0];
-
-    expandGrid.onclick = function () {
-        expanded = !expanded;
-        var grid = document.getElementsByClassName('showcase_grid')[0];
-        var showcaseOverflow = document.getElementsByClassName('showcase_overflow')[0];
-
-        if(!expanded) {
-
-            var height = grid.clientHeight;
-            showcaseOverflow.style.height = 150 + height + "px";
-            expandGrid.innerHTML = "Свернуть...";
-            expandGrid.style.padding = "5px";
-            console.log(height);
+        var height = grid.clientHeight;
+        showcaseOverflow.style.height = 150 + height + "px";
+        expandGrid.innerHTML = "Свернуть...";
+        expandGrid.style.padding = "5px";
+        console.log(height);
         }
-        else{
-            showcaseOverflow.style.height = "400px";
-            expandGrid.innerHTML = "Показать все...";
-            expandGrid.style.padding = "15px";
+    else {
+        showcaseOverflow.style.height = "400px";
+        expandGrid.innerHTML = "Показать все...";
+        expandGrid.style.padding = "15px";
         }
 
     };
-};
+
 
 
 
---
+++ js/nav.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', onScroll);
+function onScroll() {
+    var nav = document.getElementsByClassName('header_nav_main')[0];
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    scrolled !== 0 ? nav.classList.add('nav_light') : nav.classList.remove('nav_light');
+    }
